279a1f05464948c25d52ccc720a2ed2ba7b8c7ef
Elizabeth Fox <elizabethtfox93@gmail.com>
2018-03-31 17:00:43 -0400

    updated up until API call

--- assets/app.js
+++ assets/app.js
@@ -17,8 +17,8 @@ $(document).ready(function() {
   var recipes = [];
 
   //Variables for Category Ingredients
-  var dairyProducts =["2% milk","Almond Milk"]
-  var ingredientSelected;
+  var dairyProducts =["kale","AlmondMilk"]
+  var ingredientSelected = [];
 
   //Get Dairy Products and create as button
     function getDairyProducts() {
@@ -45,12 +45,16 @@ $(document).ready(function() {
         var ingrd = $('.dairyBtn').val();
         $('#selectedIngredients').append(ingrd);
         console.log(ingrd);
+
+        ingredientSelected.push(ingrd);
+        console.log(ingredientSelected);
+
     });
-  
+
     //Function for displaying initial recipe array elements as buttons
   function getRecipes() {
       var apiKey = "899742294e75cfd27b128b2974895e4d";
-      var queryURL = "https://api.edamam.com/search?q=" + recipes + "&api_key=" + apiKey + "&limit=16";
+      var queryURL = "https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?app_id=2d2c9393&q=" + ingredientSelected + "&app_key=" + apiKey + "&limit=16";
       console.log(queryURL);
 
       $.ajax({
@@ -58,42 +62,50 @@ $(document).ready(function() {
           method: "GET"
       }).done(function(response) {
           console.log(response.hits);
+          console.log(response.hits.recipe.source);
       });
-    }
 
-    //Display Recipes
+    } getRecipes();
 
+    // //Display Recipes
+    // $(document).on("click","#generateRecipe", function(event) {
+    //         getRecipes();
+    //     for (var i = 0; i < response.hits.length; i++) {
+    //         $("#apiRecipes").append(response.hits.recipe.source);
+    //         console.log(response.hits.recipe.source);
+    //     }
+    // });
 
   });
-    
-  $(document).on("click",".recipe", showRecipes);
-    // Calling the renderButtons function at least once to display the initial list of foods
-
-
-  //Grabbing and storing the data-food property value from the button
-  function showRecipes() {
-    var recipes = $(this).attr('data-name'); 
-
-    //function to prevent repeat buttons
-    $("#recipeRowOne").empty();
-    $("#recipeRowTwo").empty();
-    $("#recipeRowThree").empty();
-    $("#recipeRowFour").empty();
-    renderButtons()
-
-  //Perfoming an AJAX GET request to our queryURL      
-
-        
-
-      //Storing the array as a variable name
-      var recipeArr =response.data;
-      
-      //looping through the array in queryURL
-      for (var i=0; i<recipeArr.length; i++) {
-
-        var foodRecipes = $('<div class="col-sm-3">')
-      
-      }
-    });
-  };
-});
+//
+//   $(document).on("click",".recipe", showRecipes);
+//     // Calling the renderButtons function at least once to display the initial list of foods
+//
+//
+//   //Grabbing and storing the data-food property value from the button
+//   function showRecipes() {
+//     var recipes = $(this).attr('data-name');
+//
+//     //function to prevent repeat buttons
+//     $("#recipeRowOne").empty();
+//     $("#recipeRowTwo").empty();
+//     $("#recipeRowThree").empty();
+//     $("#recipeRowFour").empty();
+//     renderButtons()
+//
+//   //Perfoming an AJAX GET request to our queryURL
+//
+//
+//
+//       //Storing the array as a variable name
+//       var recipeArr =response.data;
+//
+//       //looping through the array in queryURL
+//       for (var i=0; i<recipeArr.length; i++) {
+//
+//         var foodRecipes = $('<div class="col-sm-3">')
+//
+//       }
+//     });
+//   };
+// });
