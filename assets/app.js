469e8fd55971073ae0282bc176c73c7e0263088f
Mariam Chaudry <mariamschaudry1@gmail.com>
2018-03-30 21:40:34 -0400

    layout

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase //
+
+var config = {
+    apiKey: "AIzaSyAvgfFYyOTbC7Bxn2JOrU54fpzw90VwGpE",
+    authDomain: "feed-me-55633.firebaseapp.com",
+    databaseURL: "https://feed-me-55633.firebaseio.com",
+    projectId: "feed-me-55633",
+    storageBucket: "",
+    messagingSenderId: "710811767309"
+  };
+  
+  firebase.initializeApp(config);
